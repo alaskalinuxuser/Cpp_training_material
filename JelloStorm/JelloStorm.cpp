4dcdc6576303e6a060002a5d5cf56910b691e926
alaskalinuxuser <westonandsuzyhoward@gmail.com>
2018-05-16 11:47:43 -0800

    Completed section 6: Added player!

--- JelloStorm/JelloStorm.cpp
+++ JelloStorm/JelloStorm.cpp
@@ -1,58 +1,65 @@
 #include <SFML/Graphics.hpp>
 #include <sstream>
 #include <SFML/Audio.hpp>
+#include <Player.h>
 
 // Declare the functions....
 
+using namespace sf;
+
 // The main process....
-int main()
-{
-	int sWidth = 1024;
-	int sHeight = 786;
-    sf::RenderWindow window(sf::VideoMode(sWidth, sHeight), "JelloStorm!");
-    
-    // Create a texture for our background and load it.
-    sf::Texture textureBackground;
+int main() {
+	
+	// What is the game state?
+	enum class State {
+		PAUSED, LEVELING_UP, GAME_OVER, PLAYING
+	};
+	
+	// We will start with the game over state.
+	State state = State::GAME_OVER; 
+	
+	// Find the screen resolution
+	Vector2f resolution;
+	//resolution.x = 800;
+	//resolution.y = 600;
+	resolution.x = VideoMode::getDesktopMode().width;
+	resolution.y = VideoMode::getDesktopMode().height;
+
+	
+	// And use it to create an SFML window
+	RenderWindow window(VideoMode(resolution.x, resolution.y), 
+		"JelloStorm!", Style::Fullscreen);
+		
+	// Create a an SFML View for the main action
+	View mainView(FloatRect(0, 0, resolution.x, resolution.y));
+	
+	// Create a texture for our background and load it.
+    Texture textureBackground;
     textureBackground.loadFromFile("graphics/background.png");
-    sf::Sprite spriteBackground;
+    
+    // Now create a sprite and attach our background texture to it.
+    Sprite spriteBackground;
     spriteBackground.setTexture(textureBackground);
+    
+    // Give the spritebackground a position.
     spriteBackground.setPosition(0,0);
-    
-    // Time control
-    sf::Clock clock;
-    
-    // Add a font.
-    sf::Font font;
-    font.loadFromFile("fonts/KOMIKAP_.ttf");
-    // Add startText
-    sf::Text startText;
-    startText.setString("Press the Enter key to start!");
-    startText.setCharacterSize(40);
-    startText.setColor(sf::Color::White);
-    startText.setFont(font);
-    // Set position based on center
-    sf::FloatRect textRect = startText.getLocalBounds();
-    startText.setOrigin(textRect.left + textRect.width / 2.0f,
-    textRect.top + textRect.height / 2.0f);
-    startText.setPosition(sWidth / 2.0f,sHeight / 2.0f);
-    
-    // Add scoreText
-    int scoreInt = 0;
-    sf::Text scoreText;
-    scoreText.setString("Score = 0");
-    scoreText.setCharacterSize(40);
-    scoreText.setColor(sf::Color::White);
-    scoreText.setFont(font);
-    scoreText.setPosition(10,10);    
-    
-    // Accept keyboard input.
-    bool acceptInput = false;    
-    
-	// Prepare the sound
-	sf::SoundBuffer chopBuffer;
-	chopBuffer.loadFromFile("sound/chop.wav");
-	sf::Sound chop;
-	chop.setBuffer(chopBuffer);
+
+	// Time control
+    Clock clock;
+
+	// Total runtime of Playing state.
+	Time gameTimeTotal;
+
+	// Where is the mouse in relation to the virtual world
+	Vector2f mouseWorldPosition;
+	// Where is the mouse in relation to the real screen
+	Vector2i mouseScreenPosition;
+	
+	// Create our player.
+	Player player;
+	
+	// Create an arena, or game area.
+	IntRect arena;
 
     while (window.isOpen()) {
 		
@@ -62,50 +69,147 @@ int main()
 		 *  Detect input.
 		 * 
 		 */
-		 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
-			// Escape key is pressed: Exit the window.
-			window.close();
-		}
 		
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return)) {
-			// Return key is pressed: pause/unpause.
-			
-			scoreInt = 0;
-			timeRemains = 6.0f;
-			gamePaused=false;
-			
-			// Enable keyboard input.
-			acceptInput = true;
-		}
-
-		if (acceptInput) {
-			
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-				
-				acceptInput = false;
-
-			}
-			
-			
-		}
-		
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Right)) {
-			// right mouse button is pressed: exit
-			window.close();
-		}
-		
-        sf::Event event;
+        Event event;        
         while (window.pollEvent(event)) {
 			
-			if (event.type == sf::Event::KeyReleased && !gamePaused) {
-				// Listen for key presses again
-				acceptInput = true;
-
-			} else if (event.type == sf::Event::Closed) {
+			// If they close the window....
+			if (event.type == Event::Closed) {
 					window.close();
                 }
-        }
+			
+			if (event.type == Event::KeyPressed) {									
+				
+				// Pause game.
+				if (event.key.code == Keyboard::Return &&
+					state == State::PLAYING)
+				{
+					state = State::PAUSED;
+				}
+
+				// Resume playing.
+				else if (event.key.code == Keyboard::Return &&
+					state == State::PAUSED)
+				{
+					state = State::PLAYING;
+					// Reset the clock so there isn't a frame jump
+					clock.restart();
+				}
+
+				// Start a new game while in GAME_OVER state
+				else if (event.key.code == Keyboard::Return &&
+					state == State::GAME_OVER)
+				{
+					state = State::LEVELING_UP;
+				}
+
+				if (state == State::PLAYING)
+				{
+				}
+
+			}
+		}// End event polling
+		
+		// Escape to quit
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+
+		// Handle controls while playing
+		if (state == State::PLAYING)
+		{
+			// Handle the pressing and releasing of the WASD keys
+			if (Keyboard::isKeyPressed(Keyboard::W))
+			{
+				player.moveUp();
+			}
+			else
+			{
+				player.stopUp();
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::S))
+			{
+				player.moveDown();
+			}
+			else
+			{
+				player.stopDown();
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::A))
+			{
+				player.moveLeft();
+			}
+			else
+			{
+				player.stopLeft();
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::D))
+			{
+				player.moveRight();
+			}
+			else
+			{
+				player.stopRight();
+			}
+
+		}// End WASD while playing
+
+		// Handle the levelling up state
+		if (state == State::LEVELING_UP)
+		{
+			// Handle the player levelling up
+			if (event.key.code == Keyboard::Num1)
+			{
+				state = State::PLAYING;
+			}
+
+			if (event.key.code == Keyboard::Num2)
+			{
+				state = State::PLAYING;
+			}
+
+			if (event.key.code == Keyboard::Num3)
+			{
+				state = State::PLAYING;
+			}
+
+			if (event.key.code == Keyboard::Num4)
+			{
+				state = State::PLAYING;
+			}
+
+			if (event.key.code == Keyboard::Num5)
+			{
+				state = State::PLAYING;
+			}
+
+			if (event.key.code == Keyboard::Num6)
+			{
+				state = State::PLAYING;
+			}
+			
+			if (state == State::PLAYING)
+			{			
+				// Prepare the next level
+				arena.width = 500;
+				arena.height = 500;
+				arena.left = 0;
+				arena.top = 0;
+
+				// We will modify this line of code later
+				int tileSize = 50;
+
+				// Spawn the player in the middle of the arena
+				player.spawn(arena, resolution, tileSize);
+				
+				// Reset the clock so there isn't a frame jump
+				clock.restart();
+			}
+		}// End levelling up
         
         /* 
 		 * 
@@ -113,10 +217,34 @@ int main()
 		 * 
 		 */
         
-        if (!gamePaused) {
-			sf::Time dt = clock.restart();
+        if (state == State::PLAYING) {
+			// Update the delta time
+			Time dt = clock.restart();
 			
-		}
+			// Update the total game time
+			gameTimeTotal += dt;
+			
+			// Make a decimal fraction of 1 from the delta time
+			float dtAsSeconds = dt.asSeconds();
+
+			// Where is the mouse pointer
+			mouseScreenPosition = Mouse::getPosition();
+
+			// Convert mouse position to world coordinates of mainView
+			mouseWorldPosition = window.mapPixelToCoords(
+				Mouse::getPosition(), mainView);
+
+			// Update the player
+			player.update(dtAsSeconds, Mouse::getPosition());
+
+			// Make a note of the players new position
+			Vector2f playerPosition(player.getCenter());
+
+			// Make the view centre around the player				
+			mainView.setCenter(player.getCenter());
+		}// End updating the scene
+		
+		
 		
 		/* 
 		 * 
@@ -124,13 +252,34 @@ int main()
 		 * 
 		 */		
 		
-		window.clear();
-		window.draw(spriteBackground);
-		
-		if (gamePaused) {
-			window.draw(startText);
+		if (state == State::PLAYING)
+		{
+			window.clear();
+
+			// set the mainView to be displayed in the window
+			// And draw everything related to it
+			window.setView(mainView);
+
+			// Draw the player
+			window.draw(player.getSprite());
 		}
-		
+
+		if (state == State::LEVELING_UP)
+		{
+			window.clear();
+		}
+
+		if (state == State::PAUSED)
+		{
+			window.clear();
+			window.draw(spriteBackground);
+		}
+
+		if (state == State::GAME_OVER)
+		{
+			window.clear();
+		}
+
 		window.display();
 		
     }
--- JelloStorm/build.sh
+++ JelloStorm/build.sh
@@ -2,8 +2,10 @@
 
 # You need libsfml installed on your system!
 
-g++ -c -std=c++11 JelloStorm.cpp
-g++ JelloStorm.o -o JelloStorm-app -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
+g++ -I./ -fpermissive -Wall -Wextra -g -c -std=c++11 Player.cpp -o Player.o
+g++ -I./ -fpermissive -Wall -Wextra -g -c -std=c++11 JelloStorm.cpp -o JelloStorm.o
+
+g++ JelloStorm.o Player.o -o JelloStorm-app -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 
 ./JelloStorm-app
 
